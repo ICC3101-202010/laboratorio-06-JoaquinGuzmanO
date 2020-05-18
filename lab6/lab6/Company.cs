ca110b2e349117e4da29968b23cd7c9f26b6765d
jaguzman <jaguzman1@miuandes.cl>
2020-05-17 23:57:51 -0400

    p1

---
+++ lab6/lab6/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6
+{
+    [Serializable]
+    class Company
+    {
+        private string name;
+        private string rut;
+
+        public Company(string name, string rut)
+        {
+            this.name = name;
+            this.rut = rut;
+        }
+        public void Info()
+        {
+            Console.WriteLine("Se creo la siguiente empresa");
+            Console.WriteLine("nombre: "+name+"\nrut: "+rut);
+        }
+    }
+}
--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
 
 namespace lab6
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Company> person_l = new List<Company>();
+            Console.WriteLine("¿Quiere utilizar un archivo para cargar la información de la empresa?[si],[no]");
+            string answer = Console.ReadLine();
+
+            if(answer == "no")
+            {
+                Console.WriteLine("diga el nombre de la empresa:");
+                string name = Console.ReadLine();
+                Console.WriteLine("diga el rut de la empresa:");
+                string rut = Console.ReadLine();
+                Company company = new Company(name, rut);
+
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, company);
+                company.Info();
+                stream.Close();
+            }
+            if(answer == "si")
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                Company company = (Company)formatter.Deserialize(stream);
+                company.Info();
+                stream.Close();
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
